4ecfd2ddc0ed9eabcb8c5754460636fb71cb6298
Zackry Stetler <zackstetler@gmail.com>
2018-07-12 21:53:09 +1200

    jQuery slider created
    Slider using jQuery now working

--- js/app.js
+++ js/app.js
@@ -1,12 +1,43 @@
 
 
-//slider
-//slideshow
+//slider logic for full width slider with structure #slider > .slides > #slide, #slide...
+const slide = (slider, width, animSpeed, pause) => {
+    const $slider = $(slider);
+    const $slides = $slider.find('.slides');
+    const $slide = $slides.children();
+    const $firstSlide = $('#slider .slides #slide:first-child');
+    $firstSlide.clone().appendTo($slides);
+
+    let currentSlide = 1;
+    let interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slides.animate({'margin-left' : `-=${width}`}, animSpeed, function() {
+                if (currentSlide === $slide.length) {
+                    $slides.css('margin-left', 0);
+                    currentSlide = 0
+                }
+                currentSlide++;
+            });
+        }, pause);
+    }
+
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+    startSlider();
+}
+
+slide('#slider', '100vw', 750, 2000);
+
 
 //parallax effect on element, typical speed between 0 and 1
 const glide = (elem, speed) => {
+    const $glide = $(elem);
     $(window).scroll(function() {
-        const $glide = $(elem);
         let windowTop = $(this).scrollTop();
         let windowBottom = windowTop + $(this).height();
     
